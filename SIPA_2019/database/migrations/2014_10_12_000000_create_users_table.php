93d8b4688edff4962a1640a660f07f1bdb176192
Lea <lea.cardenas14@gmail.com>
2019-10-01 05:08:43 -0600

    merging proyects part 1

---
+++ SIPA_2019/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sipa_usuarios_identificacion');
+            $table->string('sipa_usuarios_nombre');
+            $table->string('sipa_usuarios_apellidos');
+            $table->string('sipa_usuarios_telefono');
+            $table->string('sipa_usuarios_correo')->unique();
+            $table->integer('sipa_usuarios_unidad')->nullable();
+            $table->integer('sipa_usuarios_edificio')->nullable();
+            $table->integer('sipa_usuarios_rol')->nullable();
+            $table->integer('sipa_usuarios_usuario_creador');
+            $table->integer('sipa_usuarios_usuario_actulizacion')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ SIPA_2019/database/migrations/2019_10_01_101621_create_sipa_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSipaRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sipa_roles', function (Blueprint $table) {
+            $table->bigIncrements('sipa_roles_id');
+            $table->string('sipa_roles_codigo');
+            $table->string('sipa_roles_nombre');
+            $table->mediumText('sipa_roles_descripcion');
+            $table->integer('sipa_roles_usuario_creador');
+            $table->integer('sipa_roles_usuario_actualizacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sipa_roles');
+    }
+}
