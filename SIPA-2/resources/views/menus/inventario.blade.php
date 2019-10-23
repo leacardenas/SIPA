a7396df97fba388034d43d6421ee17246a218dbc
lea <lea.cardenas14@gmail.com>
2019-10-23 08:12:22 -0600

    Orden de menus

---
+++ SIPA-2/resources/views/menus/inventario.blade.php
@@ -0,0 +1,10 @@
+@extends('plantillas.inicio')
+@section('content')
+
+@php
+$cedula = session('idUsuario');
+$permisos = App\User::where('sipa_usuarios_identificacion',$cedula)->get()[0]->rol->permisos;
+$user = App\User::where('sipa_usuarios_identificacion',$cedula)->get()[0];
+@endphp
+
+@endsection
