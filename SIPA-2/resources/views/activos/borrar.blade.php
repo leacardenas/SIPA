c4cbb2d00478a03c5c408e03d94a313e6d3f2423
lea <lea.cardenas14@gmail.com>
2019-10-22 19:41:22 -0600

    .

--- SIPA-2/resources/views/activos/borrar.blade.php
+++ SIPA-2/resources/views/activos/borrar.blade.php
@@ -2,6 +2,10 @@
     <div class="contenidoModal" id="contenidoRegistrar">
         <span class="cerrar" onclick="cerrarModal(event, 'modalBorrarActivo')">&times;</span>
         <h1 id="registrarActivo">Borrar activo</h1>
-        Esta seguro que quiere borrar el articulo: 
+        Esta seguro que quiere borrar el articulo:
+
+        <?php
+            echo "<script>document.writeln(seleccionado);</script>";
+        ?>
     </div>
 </div>
