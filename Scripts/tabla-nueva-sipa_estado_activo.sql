55105fb4d4288bddb226f906b75f01c32f2ff024
lea <lea.cardenas14@gmail.com>
2019-11-12 19:05:21 -0600

    Scripts***

--- Scripts/tabla-nueva-sipa_estado_activo.sql
+++ Scripts/tabla-nueva-sipa_estado_activo.sql
@@ -1,60 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 10, 2019 at 10:37 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sipa`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sipa_estado_activo`
---
+ALTER TABLE `sipa_activos` CHANGE `sipa_activos_estado` `sipa_activos_estado` BIGINT(20) UNSIGNED NOT NULL;
 
 CREATE TABLE `sipa_estado_activo` (
   `sipa_estado_activo_id` int(11) NOT NULL,
-  `sipa_estado_activo_codigo` varchar(50) DEFAULT NULL,
   `sipa_estado_activo_nombre` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `sipa_estado_activo`
---
 ALTER TABLE `sipa_estado_activo`
   ADD PRIMARY KEY (`sipa_estado_activo_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `sipa_estado_activo`
---
-ALTER TABLE `sipa_estado_activo`
+  
+  ALTER TABLE `sipa_estado_activo`
   MODIFY `sipa_estado_activo_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+delete from sipa_activos;
+
+ALTER TABLE `sipa_activos` ADD CONSTRAINT `sipa_activos_estado_fk` FOREIGN KEY (`sipa_activos_estado`) REFERENCES `sipa_estado_activo`(`sipa_estado_activo_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
