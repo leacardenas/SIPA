24b8307073a26ccccb3dc3b2d995e3ee4812b2fe
Fiorella Salgado <fiorella.salgado.rodriguez@est.una.ac.cr>
2020-03-05 16:35:51 -0600

    Agregar modelo

--- SIPA-2/app/Http/Controllers/salasController.php
+++ SIPA-2/app/Http/Controllers/salasController.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\User;
 use App\Salas;
+use App\BajaSalas;
 
 class salasController extends Controller
 {
@@ -70,5 +71,9 @@ class salasController extends Controller
         }
         
         return view('salas/editar');
+    }
+
+    public function darBajaSala(Request $request){
+        
     }
 }
