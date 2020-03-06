b7fae080ee3ab79d76b3347a35f54cf90c6b7fc7
Fiorella Salgado <fiorella.salgado.rodriguez@est.una.ac.cr>
2020-03-05 18:51:04 -0600

    Hola

--- SIPA-2/resources/views/salas/darDeBajaSala.blade.php
+++ SIPA-2/resources/views/salas/darDeBajaSala.blade.php
@@ -1,7 +1,6 @@
 @extends('plantillas.inicio')
 @section('content')
 <section class="info_container">
-<<<<<<< HEAD
     <h1 class="info_h1">Dar de baja Sala</h1>
     <form method="POST" action="{{ url('/darBajaSala') }}" class="editar_sala_form" enctype="multipart/form-data">
     @csrf
@@ -12,21 +11,6 @@
     <label>Seleccione el formulario de dar de baja de esta sala</label>
     <input type="file" name="formulario_sala" required>
     <button>Dar de baja</button>
-=======
-    <h1 class="info_h1">Dar de baja <b>Sala X</b></h1>
-    <form  method="POST"  class="baja_sala_form">
-    <label class="baja_sala_label">Ingrese la razón por la que desea dar de baja esta sala</label>
-    <br>
-    <textarea class="baja_sala_input" id="info_input" id="razon_baja_sala" type="text" rows="4" cols="50" required></textarea>
-    <br>
-    <br>
-    <label class="baja_sala_label">Seleccione el formulario de dar de baja de esta sala</label>
-    <input type="file" name="formulario_sala" required></input>
-    <br>
-    <div class="button_div">
-        <button id="baja_sala_button">Dar de baja</button>
-    </div>
->>>>>>> sipa-frontend
-    </form>
+    
 </section> 
 @endsection
