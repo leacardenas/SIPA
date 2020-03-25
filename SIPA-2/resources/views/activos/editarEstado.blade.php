ff3771905cc27e99b98b3ea6bdf46500ddd23ae3
Fiorella Salgado <fiorella.salgado.rodriguez@est.una.ac.cr>
2020-03-24 19:42:08 -0600

    Actualización acumulativa

--- SIPA-2/resources/views/activos/editarEstado.blade.php
+++ SIPA-2/resources/views/activos/editarEstado.blade.php
@@ -23,7 +23,7 @@
     $estados = App\EstadoActivo::all();
     @endphp
 
-    <form id="editEstado" method="GET" action="{{ url('/editarEstado') }}" enctype="multipart/form-data">
+    <form id="editEstado" method="POST" action="{{ url('/editaEstado') }}" enctype="multipart/form-data">
     @csrf
         <div class="form-group">
             <label for="nombreActivo" id="labelNombreActivo">Seleccione el activo que desea editar</label>
