e60f2960c70cfbb56a7feac75dc7a22c1f5471a2
Rachel <rbasulto1141@hotmail.com>
2020-05-15 03:13:32 -0600

    Modal de devolucion de activo

--- SIPA-2/resources/views/inventario/activos.blade.php
+++ SIPA-2/resources/views/inventario/activos.blade.php
@@ -119,6 +119,8 @@ $permiso = App\Permiso::where('sipa_permisos_roles_role', $user->rol->sipa_roles
 </div>
 
 
+<!-- 
+
 <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" id="borrarModal">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -143,7 +145,7 @@ $permiso = App\Permiso::where('sipa_permisos_roles_role', $user->rol->sipa_roles
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 
 <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js">
